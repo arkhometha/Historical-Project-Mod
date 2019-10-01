ef147138aefa7f697c1431129d933611665e9db1
arkhometha <arkhometha@gmail.com>
2019-10-01 13:48:47 +0200

    Economy tweaks, again
    * Several small fixes for scopes, commands and a few tweaks to the economy.
    * Removed old and apparently faulty instances of "is_accepted_culture = THIS", with the country being in the scope and This referring to ThisPop.
    * Added a "cooldown" period for the debt system, to reduce the AI spamming it during large debt-payment.
    * Fix to "The Aden Protectorate"  decision not releasing Yemen by @moretrim
    * Rewrite of one Paraguyan event to guarantee no funny behaviour is going on.
    * Tweaked the value of the (HPM) mining inventions chance to happen.

--- HPM/common/defines.lua
+++ HPM/common/defines.lua
@@ -112,7 +112,7 @@ economy = {
 	SECOND_RANK_BASE_SHARE_FACTOR = 0.5,
 	CIV_BASE_SHARE_FACTOR = 0.75,
 	UNCIV_BASE_SHARE_FACTOR = 1,
-	FACTORY_PAYCHECKS_LEFTOVER_FACTOR = 0.7, -- % of how much we pay to the pops and capitalists, from the leftovers.
+	FACTORY_PAYCHECKS_LEFTOVER_FACTOR = 0.6, -- % of how much we pay to the pops and capitalists, from the leftovers.
 	MAX_FACTORY_MONEY_SAVE = 1000,	-- how much money is stored maximum in a factory.
 	SMALL_DEBT_LIMIT = 20000,
 	FACTORY_UPGRADE_EMPLOYEE_FACTOR = 0.7, -- determines how close to the employee limit we need to be before "upgrade all" will upgrade/expand a given factory (1 = 100%).
